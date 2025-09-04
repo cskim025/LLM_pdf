712edb9cbdbdb9682ef4c5f81e33d38e0ea2abd5
Changsu <110579128+cskim025@users.noreply.github.com>
2025-09-03 23:28:37 -0500

    Update execute.py

--- execute.py
+++ execute.py
@@ -1,5 +1,4 @@
-from safety_llm_pipeline import demo_pipeline
+import pipeline
 import numpy as np
 
-normal = np.load("historical_normal.npy")  # shape (n_samples, n_features)
-demo_pipeline("./safety_pdfs", normal)
+pipeline("./LLM_pdf")
